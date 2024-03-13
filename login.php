73410f980ce1a84c714382963d28d51ed0aa01cc
Tonmoy Barua <46046251+Tonmoyhridhaan@users.noreply.github.com>
2024-03-13 12:31:31 +0600

    login added

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Animated Login Form</title>
+	<title>York Burger & Pizza login</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon-32x32.png">
+	<link rel="stylesheet" href="login.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+	<div class="box">
+		<form autocomplete="off">
+			<div class="imgBox">
+				<img src="images/favicon.ico">
+			</div>
+			<h2>Sign in</h2>
+			<div class="inputBox">
+				<input type="text" required="required">
+				<span>Userame</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="password" required="required">
+				<span>Password</span>
+				<i></i>
+			</div>
+			
+			<input type="submit" value="Login">
+		</form>
+	</div>
+</body>
+</html>
